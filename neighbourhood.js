f404dc715780a9117e5d67c201f9145ad2714406
John Tajima <manjiro@gmail.com>
2009-11-15 09:57:11 -0500

    Refactor JS. Fixed ability to view in Street View

--- neighbourhood.js
+++ neighbourhood.js
@@ -1,83 +1,51 @@
 /**
- * Neighbourhood for MLS 
+ * Neighbourhood for Addresses 
  * by John Tajima
- * a jetpack addon to quickly view MLS listings in Google Maps/Bing Maps
+ * a jetpack addon to quickly view Addresses in Google Maps/Bing Maps
  */
 
-/**
- * MLS table elements
- * table id = "_ctl0_elListingSummary_rptSpecifications"
- * div.PropDetailsSummaryValue:last
- * 
- * jquery selectors aren't working though...need access to API docs
- * */
- 
 GoogleURL = "http://maps.google.com";
 BingURL   = "http://maps.bing.com";
-
 
 jetpack.future.import("menu");
 jetpack.future.import('selection');
 
 jetpack.menu.context.page.beforeShow = function(menu, context) {
-  var target = "View Address '"+jetpack.selection.text+ "'";
-  var newmenu = {
-    label: target,
-    menu: new jetpack.Menu([
-      { label: 'Google Sat. View',
-        icon: "http://www.google.com/favicon.ico",
-        command: SatView
-      },
-      { label: 'Google Street View',
-        icon: "http://www.google.com/favicon.ico",
-        command: StreetView
-      },
-      { label: "Bing Bird's Eye View",
-        icon: "http://www.bing.com/favicon.ico",
-        command: BirdView
-      }
-    ])
-  };
   menu.reset();
-  if (jetpack.selection.text) { jetpack.menu.context.page.add(newmenu); }
+  if (jetpack.selection.text) {
+    jetpack.menu.context.page.add({
+      label: "View Address '"+jetpack.selection.text)+ "'",
+      menu: new jetpack.Menu([ { label: 'Google Sat. View', icon: "http://www.google.com/favicon.ico", command: SatView },
+                               { label: 'Google Street View', icon: "http://www.google.com/favicon.ico", command: StreetView },
+                               { label: "Bing Bird's Eye View", icon: "http://www.bing.com/favicon.ico", command: BirdView } ])
+    });
+  }
 }; 
 
-
-function SatView() {
-  var params = {
-    q : jetpack.selection.text,
-    z : 18,
-    t : 'k'
-  };
-  openTab(GoogleURL, params);
+function SatView() { 
+  openTab(GoogleURL, { t: 'k', z: 18, q: jetpack.selection.text } );
 };
 
 function StreetView() {
-  var params = {
-    q : jetpack.selection.text,
-    z : 18,
-    t : 'h'
-  };
-  openTab(GoogleURL, params, "&cbp=12,0,,0,5");
+  var url = GoogleURL + "/maps/geo?"+jQuery.param({q:jetpack.selection.text});
+  $.getJSON(url, function(response){
+    try {
+      var lat = response.Placemark[0].Point.coordinates[0];
+      var lng = response.Placemark[0].Point.coordinates[1];
+      openTab(GoogleURL, { t: 'k', z:18,  layer: 'c', q: jetpack.selection.text}, "&cbp=12,0,,0,5&cbll="+[lng,lat]);
+    } catch(e) {
+      jetpack.notifications.show("Sorry, could not determine address for '"+jetpack.selection.text+"'");
+    }
+  });
 };
 
-
-//  http://www.bing.com/maps/?v=2&where1=&encType=1&style=o
 function BirdView() {
-  var params = {
-    style: 'o',     // bird's eye view
-    v: 2,
-    encType: 1,
-    where1: jetpack.selection.text
-  }
-  openTab(BingURL, params);
+  openTab(BingURL, { style: 'o', v: 2, encType: 1, where1: jetpack.selection.text });
 };
 
 function openTab(url, params, options) {
   var src = url + "?" + jQuery.param(params);
-  if (options) {
-    src += options;
-  }
+  if (options) { src += options; }
   var tab = jetpack.tabs.open(src);
   tab.focus();
 }
